99e0bc39903783b904120bbc8ba0cd77b8184b5e
George W F dos Santos <gweverton21@hotmail.com>
2020-09-15 14:03:51 -0300

    write store code for the cart

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Provider } from "react-redux";
+import Catalog from "./components/Catalog";
+
+import store from "./store";
+
+const App: React.FC = () => {
+  return (
+    <Provider store={store}>
+      <Catalog />
+    </Provider>
+  );
+};
+
+export default App;
